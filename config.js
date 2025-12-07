31fcc09698908ccfd32e77424ec79b259a07e637
mquanggiu290-dotcom <mquanggiu290@gmail.com>
2025-12-07 11:48:29 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// ไฟล์: config.js
+var myStreamConfig = {
+    // ใส่ลิงก์ m3u8 ที่นี่
+    "source": "https://jmp2.uk/plu-668c5d3bfd9eb2000882bb50.m3u8",
+    
+    // ตั้งค่าอื่นๆ
+    "autoplay": true,
+    "mute": true,
+    "logo_image": "https://via.placeholder.com/150x50?text=MyLogo", // ใส่ลิงก์โลโก้ (ถ้ามี)
+    "logo_link": "https://google.com" // ลิงก์กดที่โลโก้
+};
