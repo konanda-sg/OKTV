bcc2305efcc41c49e09e1ec088aa4fae66458bcd
mquanggiu290-dotcom <mquanggiu290@gmail.com>
2025-12-07 11:58:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // ไฟล์: config.js
 var myStreamConfig = {
     // ใส่ลิงก์ m3u8 ที่นี่
-    "source": "https://jmp2.uk/plu-668c5d3bfd9eb2000882bb50.m3u8",
+    "source": "https://stitcher-ipv4.pluto.tv/v1/stitch/embed/hls/channel/668c5d3bfd9eb2000882bb50livestitch/master.m3u8?deviceType=samsung-tvplus&deviceMake=samsung&deviceModel=samsung&deviceVersion=unknown&appVersion=unknown&deviceLat=0&deviceLon=0&deviceDNT=%7BTARGETOPT%7D&deviceId=%7BPSID%7D&advertisingId=%7BPSID%7D&us_privacy=1YNY&samsung_app_domain=%7BAPP_DOMAIN%7D&samsung_app_name=%7BAPP_NAME%7D&profileLimit=&profileFloor=&embedPartner=samsung-tvplus&profilesFromStream=true",
     
     // ตั้งค่าอื่นๆ
     "autoplay": true,
